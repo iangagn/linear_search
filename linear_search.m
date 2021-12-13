a185a70974d148ef84f64fa00c956cd0f51a02bb
Iannick Gagnon <38300786+iangagn@users.noreply.github.com>
2021-12-13 15:02:22 -0500

    Update linear_search.m

--- linear_search.m
+++ linear_search.m
@@ -1,11 +1,23 @@
 function idx = linear_search(arr, target)
 
-    for i = 1 : numel(arr)
-        if arr(i) == target
-           idx = i;
-           return
-        end
+%     Performs linear search in an array.
+%     
+%     Parameters
+%     ----------
+%     arr :
+%         A sorted integer array.
+%         
+%     target : 
+%         Value to look for in input array.
+%         
+%     Returns the index at which the value is found or -1.
+
+for i = 1 : numel(arr)
+    if arr(i) == target
+       idx = i;
+       return
     end
-    idx = -1;
+end
+idx = -1;
     
 end
